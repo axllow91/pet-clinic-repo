0299592f354bb33bea9600fa4f4854b5a608abe8
axllow91 <dobremi91@gmail.com>
2019-07-15 12:29:49 +0300

    added owner details

--- pet-clinic-web/src/main/java/com/mrn/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mrn/petclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.mrn.petclinic.controllers;
 import com.mrn.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -27,5 +30,12 @@ public class OwnerController {
     }
 
 
+    @GetMapping("{ownerId}")
+    // Represents a model and view returned by a handler, to be resolved by a DispatcherServle
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owner/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
 
 }
